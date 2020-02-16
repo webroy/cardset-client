9a11613feafdadc64d9b9ed187b554b67bc4e9c8
Roy <github@webroy.de>
2020-02-16 12:32:14 +0100

    remove unused files

--- index.php
+++ index.php
@@ -37,9 +37,6 @@
             <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
             </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="?p=home" class="nav-link">Home</a>
-            </li>
         </ul>
     </nav>
     <!-- /.navbar -->
